f9a8641d39741d8635e686214fc2b04ad0e0aa8e
Ege Girit <egegirit@gmail.com>
2022-07-16 14:45:17 +0100

    Updated plotting/Added local test results
    Updated plotting code for failure rate and added local test results for packetloss rates from 0 to 90.

--- scripts/plotting/createPlots.py
+++ scripts/plotting/createPlots.py
@@ -49,20 +49,25 @@ for x in range(0, 10):  # 10 is excluded
     print(f"  Number of packets in JSON: {packetCount}")
     # Examine all the captured packets in the JSON file
     for i in range(0, packetCount):
-        # Get latencies of the answer packets
-        # print(data[i]['_source']['layers']['dns'])
-        # To get the dns_time, the packet must have an "Answers" section
-        if 'Answers' in jsonData[i]['_source']['layers']['dns']:
-            dns_time = jsonData[i]['_source']['layers']['dns']['dns.time']
-            packetlossData[x].append(float(dns_time))
+        # Check if the packet is a DNS packet
+        if 'dns' in jsonData[i]['_source']['layers']:
+            # Get latencies of the answer packets
+            # print(data[i]['_source']['layers']['dns'])
+            # To get the dns_time, the packet must have an "Answers" section
+            if 'Answers' in jsonData[i]['_source']['layers']['dns']:
+                if 'dns.time' in jsonData[i]['_source']['layers']['dns']:
+                    dns_time = jsonData[i]['_source']['layers']['dns']['dns.time']
+                    packetlossData[x].append(float(dns_time))
             # Get failure rate (RCODE only present when there is an Answers section in the JSON)
             # count of dns.flags.rcode != 0
-            rcode = jsonData[i]['_source']['layers']['dns']['dns.flags_tree']['dns.flags.rcode']
-            # If there was an error, store 1, if not, store 0. The count of 1's is the total failure count.
-            if rcode != 0:
-                failure_rate_data[x].append(0)
-            else:
-                failure_rate_data[x].append(1)
+            if jsonData[i]['_source']['layers']['dns']['dns.flags_tree']['dns.flags.response'] == "1":
+                rcode = jsonData[i]['_source']['layers']['dns']['dns.flags_tree']['dns.flags.rcode']
+                # If there was an error, store 1, if not, store 0. The count of 1's is the total failure count.
+                failure_rate_data[x].append(int(rcode))
+                #if rcode != 0:
+                #    failure_rate_data[x].append(1)
+                #else:
+                #    failure_rate_data[x].append(0)
 
 # Create box plot for latency-packetloss
 
@@ -70,7 +75,7 @@ fig = plt.figure(figsize=(10, 7))
 
 # Creating axes instance
 ax = fig.add_axes([0, 0, 1, 1])
-ax.set_ylabel('Latency')
+ax.set_ylabel('Latency in seconds')
 ax.set_xlabel('Packetloss in percantage')
 ax.set_title('Packetloss-Latency')
 
@@ -89,13 +94,13 @@ plt.show()
 # Create bar plot for failure rate
 
 # data is defined as dictionary, key value pairs ('paketloss1' : failure rate1, ...)
-failureRateData = {'00': 0, '10': 0, '20': 0, '30': 10, '40': 20, '50': 30, '60': 50, '70': 55, '80': 70, '90': 80}
+failureRateData = {'00': 0, '10': 0, '20': 0, '30': 0, '40': 0, '50': 0, '60': 0, '70': 0, '80': 0, '90': 0}
 # The bar plot acceps a dictionary like above. This for loop extracts the saved RCODE counts and converts them to a dictionary
 for i in range(len(failure_rate_data)):
-    # print(failure_rate_data[i])
+    print(failure_rate_data[i])
     fail_count = 0
     for x in range(len(failure_rate_data[i])):
-        if failure_rate_data[i][x] == 1:
+        if failure_rate_data[i][x] != 0:
             fail_count += 1
     failureRateData[str(str(i) + str(0))] = fail_count
 

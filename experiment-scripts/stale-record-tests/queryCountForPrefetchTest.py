11ff9078ce693219ee9fe5687064e2c33ffaf458
Ege Girit <egegirit@gmail.com>
2022-10-17 00:31:33 +0100

    Update queryCountForPrefetchTest.py

--- experiment-scripts/stale-record-tests/queryCountForPrefetchTest.py
+++ experiment-scripts/stale-record-tests/queryCountForPrefetchTest.py
@@ -1,46 +1,47 @@
 from random import randrange
 
-cache_1 = 0
-cache_2 = 0
-cache_3 = 0
-cache_4 = 0
-cache_5 = 0
-
+caches = []
+max_cache_count = 5
+for i in range(max_cache_count):
+    caches.append(0)
 
 experiment_count = 100
 query_count = 30
 min_value = 1
-max_value = 5
+max_value = len(caches)
+
+print(f"Cache Length: {len(caches)}\n")
 
 cache_miss = False
 missed_experiments = []
 miss_experiment_count = 0
 
 for i in range(experiment_count):
+    cache_miss = False
     print(f"== Experiment {i+1} ==")
 
     for _ in range(query_count):
         generated_number = randrange(max_value) + min_value
-        exec(f"cache_{generated_number} += 1")
+        caches[generated_number-1] += 1
 
-    print(f"Cache 1: {cache_1}")
-    print(f"Cache 2: {cache_2}")
-    print(f"Cache 3: {cache_3}")
-    print(f"Cache 4: {cache_4}")
-    print(f"Cache 5: {cache_5}")
+    index = 0
+    for x in caches:
+        print(f"Cache {index+1}: {caches[index]}")
+        if caches[index] == 0:
+            cache_miss = True
+        index += 1
 
-    if cache_1 == 0 or cache_2 == 0 or cache_3 == 0 or cache_4 == 0 or cache_5 == 0:
-        cache_miss = True
+    if cache_miss:
         miss_experiment_count += 1
         missed_experiments.append(i)
         print(f"Cache missed! @@@@@@")
 
-    # Reset experiment
-    cache_1 = 0
-    cache_2 = 0
-    cache_3 = 0
-    cache_4 = 0
-    cache_5 = 0
+    # Reset
+    index = 0
+    for x in caches:
+        caches[index] = 0
+        index += 1
+
     print(f"\n")
 
 print(f"Cache miss: {cache_miss}")

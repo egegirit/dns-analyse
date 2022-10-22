48206513656f5f92b1e0fc03942514b91d9742f1
Ege Girit <egegirit@gmail.com>
2022-10-22 13:20:37 +0100

    Update queryCountStatistic.py

--- experiment-scripts/stale-record-tests/cache-count-experiments/queryCountStatistic.py
+++ experiment-scripts/stale-record-tests/cache-count-experiments/queryCountStatistic.py
@@ -12,13 +12,14 @@ min_value = 1
 max_value = len(caches)
 
 print(f"The resolver has {len(caches)} caches")
-print(f"We are sending {query_count} queries to the resolver\n")
+print(f"We are sending {query_count} queries to the resolver")
+print(f"Uniform distribution is assumed (Laplace)\n")
 
 print(f"Probability of Cache_i is hit with 1 query:     1/{len(caches)} = {1/len(caches)}")
 print(f"Probability of Cache_i is missed with 1 query:  1 - (1/{len(caches)}) = {1 - (1/len(caches))}\n")
 
-print(f"Probability of Cache_i is missed with {query_count} query:  {len(caches)}-1/{cache_count_of_resolver}^"
+print(f"Probability of Cache_i is missed with {query_count} query:  ({len(caches)}-1/{cache_count_of_resolver})^"
       f"{query_count} = {((len(caches) - 1) / cache_count_of_resolver)**query_count}")
 
-print(f"Probability of Cache_i is hit with {query_count} query:  1 - ({len(caches)}-1/{cache_count_of_resolver})^"
+print(f"Probability of Cache_i is hit with {query_count} query:     1 - ({len(caches)}-1/{cache_count_of_resolver})^"
       f"{query_count} = {1 - (((len(caches) - 1) / cache_count_of_resolver)**query_count)}")

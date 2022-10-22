7f3c40601a52f524bc4ca687eb4f1fc736b49faa
Ege Girit <egegirit@gmail.com>
2022-10-22 13:18:27 +0100

    Added cache count statistics for stale record experiment

---
+++ experiment-scripts/stale-record-tests/cache-count-experiments/queryCountStatistic.py
@@ -0,0 +1,24 @@
+from random import randrange
+
+cache_count_of_resolver = 18
+query_count = 40
+experiment_count = 100
+
+caches = []
+for i in range(cache_count_of_resolver):
+    caches.append(0)
+
+min_value = 1
+max_value = len(caches)
+
+print(f"The resolver has {len(caches)} caches")
+print(f"We are sending {query_count} queries to the resolver\n")
+
+print(f"Probability of Cache_i is hit with 1 query:     1/{len(caches)} = {1/len(caches)}")
+print(f"Probability of Cache_i is missed with 1 query:  1 - (1/{len(caches)}) = {1 - (1/len(caches))}\n")
+
+print(f"Probability of Cache_i is missed with {query_count} query:  {len(caches)}-1/{cache_count_of_resolver}^"
+      f"{query_count} = {((len(caches) - 1) / cache_count_of_resolver)**query_count}")
+
+print(f"Probability of Cache_i is hit with {query_count} query:  1 - ({len(caches)}-1/{cache_count_of_resolver})^"
+      f"{query_count} = {1 - (((len(caches) - 1) / cache_count_of_resolver)**query_count)}")

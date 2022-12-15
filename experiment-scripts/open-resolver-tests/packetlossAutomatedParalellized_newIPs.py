d7929d3c3ecf39cc69f31f22973fcab95d607dbb
Ege Girit <egegirit@gmail.com>
2022-12-16 00:22:09 +0100

    Added packetloss experiment with new IPs

--- experiment-scripts/open-resolver-tests/packetlossAutomatedParalellized_newIPs.py
+++ experiment-scripts/open-resolver-tests/packetlossAutomatedParalellized_newIPs.py
@@ -15,19 +15,38 @@ sleep_time = 1
 # Time to sleep between packetloss configurations. (600 seconds = 10 minutes)
 sleep_time_between_packetloss_config = 600
 
-max_worker_count = 30
-
 # Determines how many times the program sends the query
 execute_count = 1
 
 # The name of the folder that will be created to store all the tcpdump files inside it
-directory_name_of_logs = "capture_logs_truncation"
+directory_name_of_logs = "capture_logs"
 
 # Minimum and maximum counter values for the domains
-counter_min = 1  # Inclusive
+counter_min = 1   # Inclusive
 counter_max = 50  # Exclusive 
 
 # DNS Open Resolver IP Addresses
+# resolver_ip_addresses = [
+#     "94.140.14.14",  # AdGuard 1  (dns.adguard.com)
+#     "94.140.14.15",  # AdGuard 2  (dns-family.adguard.com )
+#     "185.228.168.168",  # CleanBrowsing 1  (family-filter-dns.cleanbrowsing.org )
+#     "185.228.168.9",  # CleanBrowsing 2  (security-filter-dns.cleanbrowsing.org )
+#     "1.1.1.1",  # Cloudflare 1     (one.one.one.one)
+#     "1.0.0.1",  # Cloudflare 2     (1dot1dot1dot1.cloudflare-dns.com)
+#     "216.146.35.35",  # Dyn 1  (resolver1.dyndnsinternetguide.com)
+#     "216.146.36.36",  # Dyn 2  (resolver2.dyndnsinternetguide.com )
+#     "8.8.8.8",  # Google 1  (dns.google )
+#     "8.8.4.4",  # Google 2  (dns.google )
+#     "64.6.64.6",  # Neustar 1  (?)  ERROR
+#     "156.154.70.1",  # Neustar 2  (?)  ERROR
+#     "208.67.222.222",  # OpenDNS 1  (dns.opendns.com )
+#     "208.67.222.2",  # OpenDNS 2  (sandbox.opendns.com )
+#     "9.9.9.9",  # Quad9 1    (dns9.quad9.net )
+#     "9.9.9.11",  # Quad9 2    (dns11.quad9.net)
+#     "77.88.8.1",  # Yandex 1   (dns.yandex.ru)
+#     "77.88.8.8",  # Yandex 2   (secondary.dns.yandex.ru )
+# ]
+
 resolver_ip_addresses = [
     "94.140.14.14",  # AdGuard_1
     "94.140.14.15",  # AdGuard_2
@@ -81,7 +100,7 @@ interface_2 = (
 interface_3 = "bond0"  # The interface of client which sends the queries
 
 # Packetloss rates to be simulated on the authoritative server
-packetloss_rates = [0, 10, 20, 30, 40, 50, 60, 70, 80, 85, 90, 95, 100]
+packetloss_rates = [0, 10, 20, 30, 40, 50, 60, 70, 80, 85, 90, 95]
 
 
 # Multiprocessing function that builds the all query names from a given IP address using a counter value
@@ -100,17 +119,17 @@ def build_and_send_query_mp(args_list):
     for c in range(counter_min, counter_max):
         ip_addr_with_dashes = ip_addr.replace(".", "-")
         query_prefix = (
-                ip_addr_with_dashes
-                + "-"
-                + str(c)
-                + "-"
-                + "pl"
-                + str(packetloss_rate)
+            ip_addr_with_dashes
+            + "-"
+            + str(c)
+            + "-"
+            + "pl"
+            + str(packetloss_rate)
         )
         query = (
-                query_prefix
-                + "."
-                + "packetloss.syssec-research.mmci.uni-saarland.de"
+            query_prefix
+            + "."
+            + "packetloss.syssec-research.mmci.uni-saarland.de"
         )
         resolver = dns.resolver.Resolver()
         # Set the resolver IP Address
@@ -126,7 +145,7 @@ def build_and_send_query_mp(args_list):
         try:
             answers = resolver.resolve(query, "A")
         except Exception:
-            print(f"      Exception or timeout occurred for {query_prefix} ")
+            print(f"      Exception or timeout occured for {query_prefix} ")
             answers = None
         measured_time = time.time() - start_time
         print(f"      Response time of {query_prefix}: {measured_time}")
@@ -151,23 +170,27 @@ def build_and_send_query_mp(args_list):
 # Returns true if no exception occurred. False, if subprocess.run() created an exception.
 def disable_packetloss_simulation(packetloss_rate, interface_name):
     print(f"  Disabling packetloss on {interface_name} interface with following commands:")
-    disable_packetloss_1 = f'sudo iptables-legacy -D INPUT -d 139.19.117.11/32 --protocol udp --match udp --dport 53 --match statistic --mode random --probability {packetloss_rate / 100} --match comment --comment "Random packetloss for Ege Girit Bachelor" --jump DROP'
+    disable_packetloss_1 = f'sudo iptables-legacy -D INPUT -d 139.19.117.11/32 --protocol tcp --match tcp --dport 53 --match statistic --mode random --probability {packetloss_rate / 100} --match comment --comment "Random packetloss for Ege Girit Bachelor" --jump DROP'
+    disable_packetloss_2 = f'sudo iptables-legacy -D INPUT -d 139.19.117.11/32 --protocol udp --match udp --dport 53 --match statistic --mode random --probability {packetloss_rate / 100} --match comment --comment "Random packetloss for Ege Girit Bachelor" --jump DROP'
     print("    " + disable_packetloss_1)
-
+    print("    " + disable_packetloss_2)
     try:
         subprocess.run(
             disable_packetloss_1, shell=True, stdout=subprocess.PIPE, check=True
         )
+        subprocess.run(
+            disable_packetloss_2, shell=True, stdout=subprocess.PIPE, check=True
+        )
         return True
     except Exception:
         print(
-            f"  Exception occurred while removing {current_packetloss_rate}% packetloss rule on interface {interface_name} !!"
+            f"  Exception occured while removing {current_packetloss_rate}% packetloss rule on interface {interface_name} !!"
         )
         return False
 
 
 # Sleep for a duration and show the remaining time on the console
-def sleep_for_seconds(sleep_time_between_packetloss_config):
+def sleep_for_seconds(sleep_time_between_packetloss_config):    
     print("  Remaining time:")
     # Output how many seconds left to sleep
     for i in range(sleep_time_between_packetloss_config, 0, -1):
@@ -188,26 +211,27 @@ def compress_log_files(directory_name_of_logs):
     except Exception:
         print("  Exception occurred while compressing the packet capture files !!")
 
-
 # Simulate packetloss with iptables, in case of an exception, the code attempts to remove the rule
 # Returns True when no error.
 # Use `run()` with `check=True` when setting and deleting packetloss
 # Otherwise process might not have finished before the next code runs
 def simulate_packetloss(packetloss_rate, interface_name):
-    packetloss_filter_command_1 = f'sudo iptables-legacy -A INPUT -d 139.19.117.11/32 --protocol udp --match udp --dport 53 --match statistic --mode random --probability {packetloss_rate / 100} --match comment --comment "Random packetloss for Ege Girit Bachelor" --jump DROP'
+    packetloss_filter_command_1 = f'sudo iptables-legacy -A INPUT -d 139.19.117.11/32 --protocol tcp --match tcp --dport 53 --match statistic --mode random --probability {packetloss_rate / 100} --match comment --comment "Random packetloss for Ege Girit Bachelor" --jump DROP'
+    packetloss_filter_command_2 = f'sudo iptables-legacy -A INPUT -d 139.19.117.11/32 --protocol udp --match udp --dport 53 --match statistic --mode random --probability {packetloss_rate / 100} --match comment --comment "Random packetloss for Ege Girit Bachelor" --jump DROP'
     print(
         f"  Simulating {packetloss_rate}% packetloss on interface {interface_name} with the following command:"
     )
     print("    " + packetloss_filter_command_1)
+    print("    " + packetloss_filter_command_2)
     try:
         subprocess.run(packetloss_filter_command_1, shell=True, stdout=subprocess.PIPE, check=True)
+        subprocess.run(packetloss_filter_command_2, shell=True, stdout=subprocess.PIPE, check=True)
         return True
     except Exception:
         print(
             f"  Exception occurred while simulating {packetloss_rate}% packetloss on interface {interface_name} !!"
         )
-        print(
-            f"  Removing packetloss rule by calling disable_packetloss_simulation({packetloss_rate}, {interface_name})")
+        print(f"  Removing packetloss rule by calling disable_packetloss_simulation({packetloss_rate}, {interface_name})")
         disable_packetloss_simulation(packetloss_rate, interface_name)
         print(f"  Skipping {packetloss_rate}% packetloss configuration")
         return False
@@ -223,31 +247,42 @@ def start_packet_captures(directory_name_of_logs, current_packetloss_rate, inter
     # Packet capture on authoritative server interface without the packetloss filter
     # source port should not be 53 but random.
     # The destination port is 53, but using that would only capture incoming, not outgoing traffic
-    packet_capture_command_1 = f'sudo tcpdump -w ./{directory_name_of_logs}/tcpdump_log_auth_{interface_1}_{current_packetloss_rate}.pcap -nnn -i {interface_1} "host 139.19.117.11 and (((ip[6:2] > 0) and (not ip[6] = 64)) or port 53)"'
+    packet_capture_command_1 = f'sudo tcpdump -w ./{directory_name_of_logs}/tcpdump_log_auth1_{interface_1}_{current_packetloss_rate}.pcap -nnn -i {interface_1} "host 139.19.117.11 and (((ip[6:2] > 0) and (not ip[6] = 64)) or port 53)"'
     print(
         f"  (1) Running packet capture on {interface_1} interface with the following command:"
     )
     print("    " + packet_capture_command_1)
-
+    
+    # Packet capture on authoritative server interface with the packetloss filter applied
+    # packet_capture_command_2 = f'sudo tcpdump -w ./{directory_name_of_logs}/tcpdump_log_auth2_{interface_2}_{current_packetloss_rate}.pcap -nnn -i {interface_2} "host 139.19.117.11 and (((ip[6:2] > 0) and (not ip[6] = 64)) or port 53)"'
+    # print(
+    #     f"  (2) Running packet capture on {interface_2} interface with the following command:"
+    # )
+    # print("    " + packet_capture_command_2)
+    
     # Packet capture on client interface
-    packet_capture_command_2 = f'sudo tcpdump -w ./{directory_name_of_logs}/tcpdump_log_client_{interface_3}_{current_packetloss_rate}.pcap -nnn -i {interface_3} "host 139.19.117.1 and (((ip[6:2] > 0) and (not ip[6] = 64)) or port 53)"'
+    packet_capture_command_3 = f'sudo tcpdump -w ./{directory_name_of_logs}/tcpdump_log_client_{interface_3}_{current_packetloss_rate}.pcap -nnn -i {interface_3} "host 139.19.117.1 and (((ip[6:2] > 0) and (not ip[6] = 64)) or port 53)"'
     print(
         f"  (2) Running packet capture on {interface_3} interface with the following command:"
     )
-    print("    " + packet_capture_command_2)
-
+    print("    " + packet_capture_command_3)
+    
     # Store the process objects here and return it as output
     result_processes = []
-
+    
     try:
-        process_1 = subprocess.Popen(
+        process_2 = subprocess.Popen(
             packet_capture_command_1, shell=True, stdout=subprocess.PIPE, preexec_fn=os.setsid
         )
-        process_2 = subprocess.Popen(
-            packet_capture_command_2, shell=True, stdout=subprocess.PIPE, preexec_fn=os.setsid
-        )
-        result_processes.append(process_1)
+        # process_3 = subprocess.Popen(
+        #     packet_capture_command_2, shell=True, stdout=subprocess.PIPE, preexec_fn=os.setsid
+        # )        
+        process_4 = subprocess.Popen(
+            packet_capture_command_3, shell=True, stdout=subprocess.PIPE, preexec_fn=os.setsid
+        )    
         result_processes.append(process_2)
+        # result_processes.append(process_3)
+        result_processes.append(process_4)
     except Exception:
         print("    Packet capture failed!")
         return result_processes  # Empty list
@@ -282,9 +317,8 @@ for current_packetloss_rate in packetloss_rates:
             # if simulate_packetloss() returns false, there was an exception while simulating packetloss
             # continue with the next packetloss configuration
             continue
-
-    capture_processes = start_packet_captures(directory_name_of_logs, current_packetloss_rate, interface_1, interface_2,
-                                              interface_3)
+            
+    capture_processes = start_packet_captures(directory_name_of_logs, current_packetloss_rate, interface_1, interface_2, interface_3)        
 
     # Default value of execute_count is 1
     for exec_count in range(execute_count):
@@ -293,7 +327,7 @@ for current_packetloss_rate in packetloss_rates:
         # Measure the time of parallelization (Optional)
         start = time.perf_counter()
         # Context manager
-        with concurrent.futures.ProcessPoolExecutor(max_workers=max_worker_count) as executor:
+        with concurrent.futures.ProcessPoolExecutor() as executor:
             # Using list comprehension to build the results list
             # submit() schedules the callable to be executed and returns a 
             # future object representing the execution of the callable.
@@ -328,17 +362,17 @@ for current_packetloss_rate in packetloss_rates:
         for process in capture_processes:
             try:
                 # Send the signal to all the process groups
-                os.killpg(os.getpgid(process.pid), signal.SIGTERM)
+                os.killpg(os.getpgid(process.pid), signal.SIGTERM) 
             except Exception:
-                print(f"    Exception while terminating tcpdump")
-        print(f"    Sleeping for 1 seconds for tcpdumps to terminate")
-        sleep_for_seconds(1)
+                print(f"    Exception while terminating tcpdump") 
+        print(f"    Sleeping for 1 seconds for tcpdumps to terminate")    
+        sleep_for_seconds(1) 
 
-        # Sleep for 10 minutes between packetloss configurations
+    # Sleep for 10 minutes between packetloss configurations
     print(f"  {current_packetloss_rate}% Packetloss Config Finished")
 
     # If we are in the last iteration, no need to wait
-    if current_packetloss_rate != packetloss_rates[-1]:
+    if current_packetloss_rate != 95:
         print(
             f"  Sleeping for {sleep_time_between_packetloss_config} seconds for the next packetloss iteration."
         )

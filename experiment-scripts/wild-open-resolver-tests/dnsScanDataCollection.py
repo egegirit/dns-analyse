9a3f322348722bf6aca4cfda7bd67345268e3a96
Ege Girit <egegirit@gmail.com>
2022-12-17 21:13:21 +0100

    Update dnsScanDataCollection.py

--- experiment-scripts/wild-open-resolver-tests/dnsScanDataCollection.py
+++ experiment-scripts/wild-open-resolver-tests/dnsScanDataCollection.py
@@ -388,9 +388,10 @@ def read_single_pcap(pcap_file_name, current_pl_rate):
                 # packet.show()
 
         # See how far we are when running the script
-        if index % 200000 == 0:
+        if index % 250000 == 0:
             print(f"      Packet number: ({datetime.now()}) {index}")
             input("Press any key to continue: ")
+            print(f"Continuing...")
         index += 1
 
     # After examining all the packets in the pcap file,
